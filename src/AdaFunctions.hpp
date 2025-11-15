814b7e41a1fc877e1bcf6df9eda078c4d754477f
한진희 <0200134hjh@gmail.com>
2025-11-15 14:01:00 +0900

    Create AdaFunctions.hpp

---
+++ src/AdaFunctions.hpp
@@ -0,0 +1,16 @@
+#ifndef ADA_FUNCTIONS_HPP
+#define ADA_FUNCTIONS_HPP
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int double_val(int x) {
+    return x * 2;
+}
+
+int increment(int x) {
+    return x + 1;
+}
+
+#endif
